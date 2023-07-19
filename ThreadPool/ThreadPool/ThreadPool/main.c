e92947eddfaa38c45b3a51def7b127609f819a6d
yyl1328 <2199248961@qq.com>
2023-07-19 14:04:29 +0800

    C语言手写线程池

---
+++ ThreadPool/ThreadPool/ThreadPool/main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include "threadpool.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+void taskFunc(void* arg) {
+    int num = *(int*)arg;
+    printf("thread is working ,number = %d,tid = %ld\n", num, pthread_self());
+    sleep(1);
+}
+
+int main()
+{
+    ThreadPool* pool = threadPoolCreate(3, 10, 100);
+    for (int i = 0; i < 100; i++) {
+        int* num = (int*)malloc(sizeof(int));
+        *num = i + 100;
+        threadPoolAdd(pool, taskFunc, num);
+    }
+    sleep(10);
+    threadPoolDestroy(pool);
+    sleep(5);
+    return 0;
+}
